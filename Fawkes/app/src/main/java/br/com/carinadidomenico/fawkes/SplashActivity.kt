d7b5b21330cfb00a590a078f19a3ec4571638180
carinadidomenico <carinadidomenico@gmail.com>
2019-12-20 12:07:45 -0300

    Projeto Fawkes finalizado

--- Fawkes/app/src/main/java/br/com/carinadidomenico/fawkes/SplashActivity.kt
+++ Fawkes/app/src/main/java/br/com/carinadidomenico/fawkes/SplashActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.Window
 
 class SplashActivity : AppCompatActivity() {
 
@@ -12,16 +11,17 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
 
-        Handler().postDelayed({
 
+        Handler().postDelayed({
 
             //criando a intent
 
-            //redireciona para a login/mainActivity
+
+            //redireciona para a WebActivity
             startActivity(Intent(this@SplashActivity,WebActivity::class.java))
+
             finish()
 
         },3000)
-
     }
 }
--- Fawkes/app/src/main/java/br/com/carinadidomenico/fawkes/WebActivity.kt
+++ Fawkes/app/src/main/java/br/com/carinadidomenico/fawkes/WebActivity.kt
@@ -1,35 +1,71 @@
 package br.com.carinadidomenico.fawkes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
-import android.view.Window
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.web_activity.*
 
 class WebActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.web_activity)
 
-        wbvWeb.settings.javaScriptEnabled = true
+        wbweb.settings.javaScriptEnabled = true
+        wbweb.loadUrl("http://br.cellep.com")
 
-        wbvWeb.setWebViewClient(WebViewClient())
-        wbvWeb.loadUrl("http://br.cellep.com")
+        wbweb.webViewClient = object : WebViewClient(){
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+
+                Inpbusca.setText(url)
+
+            }
+
+        }
+
 
         btnVoltar.setOnClickListener {
-            wbvWeb.goBack()
+            wbweb.goBack()
+
         }
-        btnAvancar.setOnClickListener {
-            wbvWeb.goForward()
+
+
+        btnAvançar.setOnClickListener {
+            wbweb.goForward()
+
         }
-        btnAtt.setOnClickListener {
-            wbvWeb.reload()
+
+
+        btnRefresh.setOnClickListener {
+            wbweb.reload()
+
         }
+
         btnPesquisar.setOnClickListener {
-            wbvWeb.loadUrl(inpBusca.getText().toString())
+            var pesquisa = Inpbusca.text.toString().trim()
+
+            if (Inpbusca.text.toString().trim().indexOf("www.") < 0) {
+                pesquisa = "www.$pesquisa"
+            }
+            if (Inpbusca.text.toString().trim().indexOf("https://") < 0) {
+                pesquisa = "https://$pesquisa"
+            }
+            if (Inpbusca.text.toString().trim().indexOf(".com") < 0) {
+                pesquisa = "$pesquisa.com"
+            }
+            if (Inpbusca.text.toString().trim().isEmpty()) {
+                Toast.makeText(this@WebActivity, "Campo vazio ", Toast.LENGTH_LONG).show()
+            } else {
+                wbweb.loadUrl(pesquisa)
+                Inpbusca.setText(pesquisa)
+
+            }
+
         }
     }
-
 }
